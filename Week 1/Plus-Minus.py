55321271c11f0b78d0bd384ba8935a602a58cdb5
Cheryl O Liu <67553615+ollulceland@users.noreply.github.com>
2021-08-24 20:56:35 +0100

    Update Plus-Minus.py

--- Week 1/Plus-Minus.py
+++ Week 1/Plus-Minus.py
@@ -1,1 +1,33 @@
+#!/bin/python3
 
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the 'plusMinus' function below.
+# The function accepts INTEGER_ARRAY arr as parameter.
+
+def plusMinus(arr):
+    positive_counter = 0
+    negative_counter = 0
+    zero_counter = 0
+    
+    for i in range(len(arr)):
+        if arr[i] > 0:
+            positive_counter += 1
+        elif arr[i] < 0:
+            negative_counter += 1
+        else:
+            zero_counter += 1
+    print("%f"%(positive_counter/len(arr)))
+    print("%f"%(negative_counter/len(arr)))
+    print("%f"%(zero_counter/len(arr))) 
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
